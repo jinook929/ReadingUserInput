2861d5ece91110b62f4af0eefc34aa5db11bb875
jinook929 <jinook929@gmail.com>
2019-10-12 22:52:55 -0400

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,23 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); // working as ENTER key
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        int age = 2019 - yearOfBirth;
+
+        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+
+        scanner.close();
+    }
+}
